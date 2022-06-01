6fba0e880d9f300c7412b8d5d46e8558d56008b2
DenisStoimenov <96194514+DenisStoimenov@users.noreply.github.com>
2022-06-01 16:23:36 +0200

    thats it for today

---
+++ day2/os.js
@@ -0,0 +1,9 @@
+const os = require('os');
+
+const totalMemory = os.totalmem();
+const freeMemory = os.freemem();
+
+console.log('Total memory: ' + totalMemory);
+console.log(`Free memory: ${freeMemory}`);
+
+console.log(`Free memory percentage ${freeMemory / totalMemory * 100}%`);
